801438baca366b97cafb4ce99b982efec66a4882
Andrew Whitehead <cywolf@gmail.com>
2020-02-06 16:10:36 -0800

    add python package metadata
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

---
+++ wrappers/python/setup.py
@@ -0,0 +1,24 @@
+"""Module setup."""
+
+import runpy
+from setuptools import find_packages, setup
+
+PACKAGE_NAME = "indy_vdr"
+version_meta = runpy.run_path("./{}/version.py".format(PACKAGE_NAME))
+VERSION = version_meta["__version__"]
+
+if __name__ == "__main__":
+    setup(
+        name=PACKAGE_NAME,
+        version=VERSION,
+        url="https://github.com/andrewwhitehead/indy-vdr",
+        packages=find_packages(),
+        include_package_data=True,
+        package_data={"indy_vdr": []},
+        python_requires=">=3.6.3",
+        classifiers=[
+            "Programming Language :: Python :: 3",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+    )
