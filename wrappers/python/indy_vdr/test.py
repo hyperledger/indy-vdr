e4f67840275329caebfeb8ce507ab27e954bfeec
Andrew Whitehead <cywolf@gmail.com>
2020-02-06 16:14:49 -0800

    update python test script
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- wrappers/python/indy_vdr/test.py
+++ wrappers/python/indy_vdr/test.py
@@ -1,7 +1,9 @@
 import asyncio
 import json
+import sys
+
 from .bindings import get_version
-from .ledger import CustomRequest, LedgerType
+from .ledger import CustomRequest, GetTxnRequest, GetValidatorInfoRequest, LedgerType
 from .pool import Pool
 
 
@@ -14,29 +16,38 @@ async def get_pool_txns():
         print(txn)
 
 
-async def get_txn(seq_no: int):
-    req = {"operation": {"data": seq_no, "ledgerId": LedgerType.DOMAIN, "type": "3"}}
-    result = await pool.submit_request(req)
-    body = json.loads(result)["result"]
-    return body
+async def get_txn(pool: Pool, seq_no: int):
+    req = GetTxnRequest(LedgerType.DOMAIN, seq_no)
+    return await pool.submit_request(req)
 
 
-async def get_txn_range(seq_nos):
-    return [await get_txn(seq_no) for seq_no in seq_nos]
+async def get_txn_range(pool: Pool, seq_nos):
+    return [await get_txn(pool, seq_no) for seq_no in seq_nos]
 
 
-log("indy-vdr version:", get_version())
+async def get_validator_info(pool: Pool):
+    req = GetValidatorInfoRequest("V4SGRU86Z58d6TV7PBUe6f")
+    return await pool.submit_action(req)
 
-test_req = {"operation": {"data": 1, "ledgerId": 1, "type": "3"}}
 
-req = CustomRequest(test_req)
-log("Request body:", req.body)
+if __name__ == "__main__":
+    log("indy-vdr version:", get_version())
 
-sig_in = req.signature_input
-log("Signature input:", sig_in)
+    genesis_path = len(sys.argv) > 1 and sys.argv[1] or "genesis.txn"
 
-pool = Pool(genesis_path="../../bctest.txn")
-log(f"Created pool {pool}")
+    pool = Pool(genesis_path="genesis.txn")
+    log(f"Created pool: {pool}")
 
-txn = asyncio.get_event_loop().run_until_complete(get_txn(11))
-log(json.dumps(txn, indent=2))
+    test_req = {"operation": {"data": 1, "ledgerId": 1, "type": "3"}}
+    req = CustomRequest(test_req)
+    log("Custom request body:", req.body)
+
+    sig_in = req.signature_input
+    log("Custom request signature input:", sig_in)
+
+    print("Refresh pool..")
+    asyncio.get_event_loop().run_until_complete(pool.refresh())
+    print()
+
+    txn = asyncio.get_event_loop().run_until_complete(get_txn(pool, 11))
+    log(json.dumps(txn, indent=2))
