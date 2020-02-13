09d24cd3237d2d695c9f0b37e992d567f9080102
Andrew Whitehead <cywolf@gmail.com>
2020-02-13 10:57:38 -0800

    export open_pool
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- wrappers/python/indy_vdr/__init__.py
+++ wrappers/python/indy_vdr/__init__.py
@@ -1,10 +1,11 @@
 from .bindings import set_config, set_protocol_version
 from .error import VdrError, VdrErrorCode
 from .ledger import LedgerType
-from .pool import Pool
+from .pool import open_pool, Pool
 from .request import Request
 
 __all__ = [
+    "open_pool",
     "set_config",
     "set_protocol_version",
     "LedgerType",
