6550212d630c7f4187e4b6835878fca3f31923d5
artem.ivanov <artem.ivanov@dsr-company.com>
2020-03-05 19:15:01 +0300

    IS-1501:Libindy use Indy VDR Part 2
    Signed-off-by: artem.ivanov <artem.ivanov@dsr-company.com>

--- libindy_vdr/src/ledger/requests/rev_reg.rs
+++ libindy_vdr/src/ledger/requests/rev_reg.rs
@@ -32,6 +32,14 @@ pub enum RevocationRegistryDelta {
     RevocationRegistryDeltaV1(RevocationRegistryDeltaV1),
 }
 
+impl From<RevocationRegistryDelta> for RevocationRegistryDeltaV1 {
+    fn from(rev_reg_delta: RevocationRegistryDelta) -> Self {
+        match rev_reg_delta {
+            RevocationRegistryDelta::RevocationRegistryDeltaV1(rev_reg_delta) => rev_reg_delta,
+        }
+    }
+}
+
 impl Validatable for RevocationRegistryDelta {}
 
 #[derive(Debug, Serialize, Deserialize)]
