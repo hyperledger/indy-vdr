987bd35204401197e9ce241ad97fae576302be2a
Dominic Wörner <dom.woe@gmail.com>
2022-02-21 10:48:57 +0100

    did:indy resolver
    Signed-off-by: Dominic Wörner <dom.woe@gmail.com>

---
+++ libindy_vdr/src/resolver/resolver.rs
@@ -0,0 +1,336 @@
+use chrono::{DateTime, Utc};
+
+use crate::utils::Qualifiable;
+use futures_executor::block_on;
+use serde_json::Value;
+
+use super::did::{DidUrl, LedgerObject, QueryParameter};
+use super::did_document::{DidDocument, LEGACY_INDY_SERVICE};
+use crate::common::error::prelude::*;
+use crate::ledger::responses::{Endpoint, GetNymResultV1};
+
+use crate::ledger::constants;
+use crate::ledger::identifiers::{CredentialDefinitionId, RevocationRegistryId, SchemaId};
+use crate::ledger::RequestBuilder;
+use crate::pool::helpers::perform_ledger_request;
+use crate::pool::{Pool, PreparedRequest, RequestResult, TimingResult};
+use crate::utils::did::DidValue;
+
+// DID (URL) Resolver for did:indy method
+pub struct Resolver<T: Pool> {
+    pool: T,
+}
+
+impl<T: Pool> Resolver<T> {
+    pub fn new(pool: T) -> Resolver<T> {
+        Resolver { pool }
+    }
+
+    pub fn resolve(&self, did: &str) -> VdrResult<String> {
+        let did_url = DidUrl::from_str(did)?;
+
+        let builder = self.pool.get_request_builder();
+        let request = build_request(&did_url, &builder)?;
+
+        let ledger_data = handle_request(&self.pool, &request)?;
+        let data = parse_ledger_data(&ledger_data)?;
+
+        let result = match request.txn_type.as_str() {
+            constants::GET_NYM => {
+                let get_nym_result: GetNymResultV1 =
+                    serde_json::from_str(data.as_str().unwrap())
+                        .map_err(|_| err_msg(VdrErrorKind::Resolver, "Could not parse NYM data"))?;
+
+                let endpoint: Option<Endpoint> = if get_nym_result.diddoc_content.is_none() {
+                    // Legacy: Try to find an attached ATTRIBUTE transacation with raw endpoint
+                    self.fetch_legacy_endpoint(&did_url.id).ok()
+                } else {
+                    None
+                };
+
+                let did_document = DidDocument::new(
+                    &did_url.namespace,
+                    &get_nym_result.dest,
+                    &get_nym_result.verkey,
+                    endpoint,
+                    None,
+                );
+                did_document.to_string()?
+            }
+            // other ledger objects
+            _ => data.to_string(),
+        };
+
+        Ok(result)
+    }
+
+    fn fetch_legacy_endpoint(&self, did: &DidValue) -> VdrResult<Endpoint> {
+        let builder = self.pool.get_request_builder();
+        let request = builder.build_get_attrib_request(
+            None,
+            did,
+            Some(String::from(LEGACY_INDY_SERVICE)),
+            None,
+            None,
+        )?;
+        let ledger_data = handle_request(&self.pool, &request)?;
+        let endpoint_data = parse_ledger_data(&ledger_data)?;
+        let endpoint_data: Endpoint = serde_json::from_str(endpoint_data.as_str().unwrap())
+            .map_err(|_| err_msg(VdrErrorKind::Resolver, "Could not parse endpoint data"))?;
+        Ok(endpoint_data)
+    }
+}
+
+fn build_request(did: &DidUrl, builder: &RequestBuilder) -> VdrResult<PreparedRequest> {
+    let request = if did.path.is_some() {
+        match LedgerObject::from_str(did.path.as_ref().unwrap().as_str())? {
+            LedgerObject::Schema(schema) => builder.build_get_schema_request(
+                None,
+                &SchemaId::new(&did.id, &schema.name, &schema.version),
+            ),
+            LedgerObject::ClaimDef(claim_def) => builder.build_get_cred_def_request(
+                None,
+                &CredentialDefinitionId::from_str(
+                    format!(
+                        "{}:3:CL:{}:{}",
+                        &did.id, claim_def.schema_seq_no, claim_def.name
+                    )
+                    .as_str(),
+                )
+                .unwrap(),
+            ),
+            LedgerObject::RevRegDef(rev_reg_def) => builder.build_get_revoc_reg_def_request(
+                None,
+                &RevocationRegistryId::from_str(
+                    format!(
+                        "{}:4:{}:3:CL:{}:{}:CL_ACCUM:{}",
+                        &did.id,
+                        &did.id,
+                        rev_reg_def.schema_seq_no,
+                        rev_reg_def.claim_def_name,
+                        rev_reg_def.tag
+                    )
+                    .as_str(),
+                )
+                .unwrap(),
+            ),
+            LedgerObject::RevRegEntry(rev_reg_entry) => {
+                let timestamp = parse_or_now(did.query.get(&QueryParameter::VersionTime))?;
+
+                builder.build_get_revoc_reg_request(
+                    None,
+                    &RevocationRegistryId::from_str(
+                        format!(
+                            "{}:4:{}:3:CL:{}:{}:CL_ACCUM:{}",
+                            &did.id,
+                            &did.id,
+                            rev_reg_entry.schema_seq_no,
+                            rev_reg_entry.claim_def_name,
+                            rev_reg_entry.tag
+                        )
+                        .as_str(),
+                    )
+                    .unwrap(),
+                    timestamp,
+                )
+            }
+            LedgerObject::RevRegDelta(rev_reg_delta) => {
+                let mut from: Option<i64> = None;
+                if did.query.contains_key(&QueryParameter::From) {
+                    from = did
+                        .query
+                        .get(&QueryParameter::From)
+                        .and_then(|d| DateTime::parse_from_rfc3339(d).ok())
+                        .and_then(|d| Some(d.timestamp()));
+                }
+
+                let to = parse_or_now(did.query.get(&QueryParameter::To))?;
+
+                builder.build_get_revoc_reg_delta_request(
+                    None,
+                    &RevocationRegistryId::from_str(
+                        format!(
+                            "{}:4:{}:3:CL:{}:{}:CL_ACCUM:{}",
+                            &did.id,
+                            &did.id,
+                            rev_reg_delta.schema_seq_no,
+                            rev_reg_delta.claim_def_name,
+                            rev_reg_delta.tag
+                        )
+                        .as_str(),
+                    )
+                    .unwrap(),
+                    from,
+                    to,
+                )
+            }
+        }
+    } else {
+        // TODO: Use with new get nym request
+
+        let _seq_no: Option<i64> = did
+            .query
+            .get(&QueryParameter::VersionId)
+            .and_then(|v| v.parse().ok());
+        let _timestamp: Option<i64> = did
+            .query
+            .get(&QueryParameter::VersionTime)
+            .and_then(|d| DateTime::parse_from_rfc3339(d).ok())
+            .and_then(|d| Some(d.timestamp()));
+
+        builder.build_get_nym_request(Option::None, &did.id)
+    };
+    request
+}
+
+fn handle_request<T: Pool>(pool: &T, request: &PreparedRequest) -> VdrResult<String> {
+    let (result, _timing) = block_on(request_transaction(pool, &request))?;
+    match result {
+        RequestResult::Reply(data) => Ok(data),
+        RequestResult::Failed(error) => Err(error),
+    }
+}
+
+async fn request_transaction<T: Pool>(
+    pool: &T,
+    request: &PreparedRequest,
+) -> VdrResult<(RequestResult<String>, Option<TimingResult>)> {
+    perform_ledger_request(pool, &request).await
+}
+
+fn parse_ledger_data(ledger_data: &str) -> VdrResult<Value> {
+    let v: Value = serde_json::from_str(&ledger_data)
+        .map_err(|_| err_msg(VdrErrorKind::Resolver, "Could not parse ledger response"))?;
+    let data: &Value = &v["result"]["data"];
+    if *data == Value::Null {
+        Err(err_msg(
+            VdrErrorKind::Resolver,
+            format!("Empty data in ledger response"),
+        ))
+    } else {
+        Ok(data.to_owned())
+    }
+}
+
+fn parse_or_now(datetime: Option<&String>) -> VdrResult<i64> {
+    match datetime {
+        Some(datetime) => {
+            let dt = DateTime::parse_from_rfc3339(datetime).map_err(|_| {
+                err_msg(
+                    VdrErrorKind::Resolver,
+                    format!("Could not parse datetime {}", datetime),
+                )
+            })?;
+            Ok(dt.timestamp())
+        }
+        None => Ok(Utc::now().timestamp()),
+    }
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::*;
+    use rstest::*;
+
+    use crate::pool::ProtocolVersion;
+
+    #[fixture]
+    fn request_builder() -> RequestBuilder {
+        RequestBuilder::new(ProtocolVersion::Node1_4)
+    }
+
+    #[rstest]
+    fn build_get_revoc_reg_request_from_version_time(request_builder: RequestBuilder) {
+        let datetime_as_str = "2020-12-20T19:17:47Z";
+        let did_url_as_str = format!("did:indy:idunion:Dk1fRRTtNazyMuK2cr64wp/anoncreds/v0/REV_REG_ENTRY/104/revocable/a4e25e54?versionTime={}",datetime_as_str);
+        let did_url = DidUrl::from_str(&did_url_as_str).unwrap();
+        let request = build_request(&did_url, &request_builder).unwrap();
+        let timestamp = (*(request.req_json).get("operation").unwrap())
+            .get("timestamp")
+            .unwrap()
+            .as_u64()
+            .unwrap() as i64;
+        assert_eq!(constants::GET_REVOC_REG, request.txn_type);
+
+        assert_eq!(
+            DateTime::parse_from_rfc3339(datetime_as_str)
+                .unwrap()
+                .timestamp(),
+            timestamp
+        );
+    }
+
+    #[rstest]
+    fn build_get_revoc_reg_without_version_time(request_builder: RequestBuilder) {
+        let now = chrono::Utc::now().timestamp();
+
+        let did_url_as_str = "did:indy:idunion:Dk1fRRTtNazyMuK2cr64wp/anoncreds/v0/REV_REG_ENTRY/104/revocable/a4e25e54";
+        let did_url = DidUrl::from_str(did_url_as_str).unwrap();
+        let request = build_request(&did_url, &request_builder).unwrap();
+        let timestamp = (*(request.req_json).get("operation").unwrap())
+            .get("timestamp")
+            .unwrap()
+            .as_u64()
+            .unwrap() as i64;
+
+        assert_eq!(constants::GET_REVOC_REG, request.txn_type);
+        assert!(timestamp >= now);
+    }
+
+    #[rstest]
+    fn build_get_revoc_reg_request_fails_with_unparsable_version_time(
+        request_builder: RequestBuilder,
+    ) {
+        let datetime_as_str = "20201220T19:17:47Z";
+        let did_url_as_str = format!("did:indy:idunion:Dk1fRRTtNazyMuK2cr64wp/anoncreds/v0/REV_REG_ENTRY/104/revocable/a4e25e54?versionTime={}",datetime_as_str);
+        let did_url = DidUrl::from_str(&did_url_as_str).unwrap();
+        let _err = build_request(&did_url, &request_builder).unwrap_err();
+    }
+
+    #[rstest]
+    fn build_get_revoc_reg_delta_request_with_from_to(request_builder: RequestBuilder) {
+        let from_as_str = "2019-12-20T19:17:47Z";
+        let to_as_str = "2020-12-20T19:17:47Z";
+        let did_url_as_str = format!("did:indy:idunion:Dk1fRRTtNazyMuK2cr64wp/anoncreds/v0/REV_REG_DELTA/104/revocable/a4e25e54?from={}&to={}",from_as_str, to_as_str);
+        let did_url = DidUrl::from_str(&did_url_as_str).unwrap();
+        let request = build_request(&did_url, &request_builder).unwrap();
+        assert_eq!(request.txn_type, constants::GET_REVOC_REG_DELTA);
+    }
+
+    #[rstest]
+    fn build_get_revoc_reg_delta_request_with_from_only(request_builder: RequestBuilder) {
+        let now = chrono::Utc::now().timestamp();
+        let from_as_str = "2019-12-20T19:17:47Z";
+        let did_url_as_str = format!("did:indy:idunion:Dk1fRRTtNazyMuK2cr64wp/anoncreds/v0/REV_REG_DELTA/104/revocable/a4e25e54?from={}",from_as_str);
+        let did_url = DidUrl::from_str(&did_url_as_str).unwrap();
+        let request = build_request(&did_url, &request_builder).unwrap();
+
+        let to = (*(request.req_json).get("operation").unwrap())
+            .get("to")
+            .unwrap()
+            .as_u64()
+            .unwrap() as i64;
+        assert_eq!(request.txn_type, constants::GET_REVOC_REG_DELTA);
+        assert!(to >= now)
+    }
+
+    #[rstest]
+    fn build_get_revoc_reg_delta_request_without_parameter(request_builder: RequestBuilder) {
+        let now = chrono::Utc::now().timestamp();
+        let did_url_as_str = "did:indy:idunion:Dk1fRRTtNazyMuK2cr64wp/anoncreds/v0/REV_REG_DELTA/104/revocable/a4e25e54";
+        let did_url = DidUrl::from_str(did_url_as_str).unwrap();
+        let request = build_request(&did_url, &request_builder).unwrap();
+
+        let to = (*(request.req_json).get("operation").unwrap())
+            .get("to")
+            .unwrap()
+            .as_u64()
+            .unwrap() as i64;
+
+        let from = (*(request.req_json).get("operation").unwrap()).get("from");
+        assert_eq!(request.txn_type, constants::GET_REVOC_REG_DELTA);
+        assert!(from.is_none());
+        assert!(to >= now);
+    }
+}
