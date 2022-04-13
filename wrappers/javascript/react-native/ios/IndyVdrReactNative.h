a13d5effbbeabb43807491ec81719f313b3b65d0
blu3beri <berend@animo.id>
2022-04-13 12:00:16 +0200

    feat(js): added ios setup partially
    Signed-off-by: blu3beri <berend@animo.id>
    Signed-off-by: Berend Sliedrecht <berend@animo.id>
    Signed-off-by: Christopher Hempel <c.hempel@esatus.com>

--- wrappers/javascript/react-native/ios/IndyVdrReactNative.h
+++ wrappers/javascript/react-native/ios/IndyVdrReactNative.h
@@ -1,11 +1,5 @@
 #import <React/RCTBridgeModule.h>
 
-#ifdef __cplusplus
-
-#import "react-native-indy-vdr-react-native.h"
-
-#endif
-
 @interface IndyVdrReactNative : NSObject <RCTBridgeModule>
 
 @end
