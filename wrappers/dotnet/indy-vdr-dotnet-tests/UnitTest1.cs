60add88959c0cda1a65fde50b6646563c76f72b6
Jan Wameling <j.wameling@esatus.com>
2022-05-23 15:03:16 +0200

    init

---
+++ wrappers/dotnet/indy-vdr-dotnet-tests/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace indy_vdr_dotnet_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
