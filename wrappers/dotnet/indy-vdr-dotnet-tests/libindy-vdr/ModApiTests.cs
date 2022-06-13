c335c4a30c04d94ffe163a78e5f19ef922478ffc
Jan Wameling <j.wameling@esatus.com>
2022-06-13 09:41:02 +0200

    commented out old tests

--- wrappers/dotnet/indy-vdr-dotnet-tests/libindy-vdr/ModApiTests.cs
+++ wrappers/dotnet/indy-vdr-dotnet-tests/libindy-vdr/ModApiTests.cs
@@ -8,70 +8,70 @@ namespace indy_vdr_dotnet_tests.libindy_vdr
 {
     public class ModApiTests
     {
-        [Test]
-        [TestCase(TestName = "GetVersionAsync returns a string that is not empty.")]
-        public async Task GetVersion()
-        {
-            //Arrange
+        //[Test]
+        //[TestCase(TestName = "GetVersionAsync returns a string that is not empty.")]
+        //public async Task GetVersion()
+        //{
+        //    //Arrange
 
-            //Act
-            string actual = await Mod.GetVersionAsync();
+        //    //Act
+        //    string actual = await Mod.GetVersionAsync();
 
-            //Assert
-            actual.Should().NotBeEmpty();
-            Console.WriteLine(actual);
-        }
+        //    //Assert
+        //    actual.Should().NotBeEmpty();
+        //    Console.WriteLine(actual);
+        //}
 
-        [Test]
-        [TestCase(TestName = "SetConfig does not throw an exception.")]
-        public void SetConfig()
-        {
-            //Arrange
+        //[Test]
+        //[TestCase(TestName = "SetConfig does not throw an exception.")]
+        //public void SetConfig()
+        //{
+        //    //Arrange
 
-            //Act
-            Action act = () => Mod.SetConfig("config");
+        //    //Act
+        //    Action act = () => Mod.SetConfig("config");
 
-            //Assert
-            act.Should().NotThrow();
-        }
+        //    //Assert
+        //    act.Should().NotThrow();
+        //}
 
-        [Test]
-        [TestCase(TestName = "SetDefaultLogger does not throw an exception.")]
-        public void SetDefaultLogger()
-        {
-            //Arrange
+        //[Test]
+        //[TestCase(TestName = "SetDefaultLogger does not throw an exception.")]
+        //public void SetDefaultLogger()
+        //{
+        //    //Arrange
 
-            //Act
-            Action act = () => Mod.SetDefaultLogger();
+        //    //Act
+        //    Action act = () => Mod.SetDefaultLogger();
 
-            //Assert
-            act.Should().NotThrow();
-        }
+        //    //Assert
+        //    act.Should().NotThrow();
+        //}
 
-        [Test]
-        [TestCase(TestName = "SetSocksProxy does not throw an exception.")]
-        public void SetSocksProxy()
-        {
-            //Arrange
+        //[Test]
+        //[TestCase(TestName = "SetSocksProxy does not throw an exception.")]
+        //public void SetSocksProxy()
+        //{
+        //    //Arrange
 
-            //Act
-            Action act = () => Mod.SetSocksProxy("socks_proxy");
+        //    //Act
+        //    Action act = () => ModApi.SetSocksProxy("socks_proxy");
 
-            //Assert
-            act.Should().NotThrow();
-        }
+        //    //Assert
+        //    act.Should().NotThrow();
+        //}
 
-        [Test]
-        [TestCase(TestName = "SetProtocolVersion does not throw an exception.")]
-        public void SetProtocolVersion()
-        {
-            //Arrange
+        //[Test]
+        //[TestCase(TestName = "SetProtocolVersion does not throw an exception.")]
+        //public void SetProtocolVersion()
+        //{
+        //    //Arrange
 
-            //Act
-            Action act = () => Mod.SetProtocolVersion(1u);
+        //    //Act
+        //    Action act = () => Mod.SetProtocolVersion(1u);
 
-            //Assert
-            act.Should().NotThrow();
-        }
+        //    //Assert
+        //    act.Should().NotThrow();
+        //}
     }
 }
