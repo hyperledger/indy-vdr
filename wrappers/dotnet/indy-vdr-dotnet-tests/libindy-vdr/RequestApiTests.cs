3cafe224f17ca1c56a3026d99296cf0ffbe551e9
Jan Wameling <j.wameling@esatus.com>
2022-06-17 09:03:39 +0200

    added wrapper methods in requestApi

--- wrappers/dotnet/indy-vdr-dotnet-tests/libindy-vdr/RequestApiTests.cs
+++ wrappers/dotnet/indy-vdr-dotnet-tests/libindy-vdr/RequestApiTests.cs
@@ -1,23 +1,35 @@
 ﻿using FluentAssertions;
 using indy_vdr_dotnet.libindy_vdr;
 using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
 
 namespace indy_vdr_dotnet_tests.libindy_vdr
 {
     public class RequestApiTests
     {
-        //[Test]
-        //[TestCase(TestName = "PrepareTxnAuthorAgreementAcceptance")]
-        //public void PrepareTxnAuthorAgreementAcceptance()
-        //{
-        //    //Arrange
+        [Test]
+        [TestCase(TestName = "PrepareTxnAuthorAgreementAcceptance")]
+        public async Task PrepareTxnAuthorAgreementAcceptanceWorks()
+        {
+            //Arrange
+            string expected = "";
+            string testAccMechType = "acc_mech_type";
+            ulong testTime = (ulong) DateTimeOffset.Now.ToUnixTimeSeconds();
+            string testText = "text";
+            string testVersion = "version";
+            string testTaaDigest = "taa_digest";
+            //Act
 
-        //    //Act
-        //    string expected = "";
-        //    string actual = RequestApi.PrepareTxnAuthorAgreementAcceptance("acc_mech_type", 10u, "text", "version", "taa_digest").GetAwaiter().GetResult();
+            string actual = await RequestApi.PrepareTxnAuthorAgreementAcceptance(
+                testAccMechType,
+                testTime,
+                testText,
+                testVersion,
+                testTaaDigest);
 
-        //    //Assert
-        //    actual.Should().Be(expected);
-        //}
+            //Assert
+            actual.Should().NotBe(expected);
+        }
     }
 }
--- wrappers/dotnet/indy-vdr-dotnet/libindy-vdr/RequestApi.cs
+++ wrappers/dotnet/indy-vdr-dotnet/libindy-vdr/RequestApi.cs
@@ -1,13 +1,150 @@
-﻿namespace indy_vdr_dotnet.libindy_vdr
+﻿using System;
+using System.Threading.Tasks;
+using static indy_vdr_dotnet.models.Structures;
+
+namespace indy_vdr_dotnet.libindy_vdr
 {
     public static class RequestApi
     {
-        //public static Task<string> PrepareTxnAuthorAgreementAcceptance(string acc_mech_type, uint time, string text = null, string version = null, string taa_digest = null)
-        //{
-        //    string result = "";
-        //    NativeMethods.indy_vdr_prepare_txn_author_agreement_acceptance(text, version, taa_digest, acc_mech_type, time, ref result);
-        //    return Task.FromResult(result);
-        //}
+        public static async Task<string> PrepareTxnAuthorAgreementAcceptance(
+            string accMechType,
+            ulong time,
+            string text = null,
+            string version = null,
+            string taaDigest = null)
+        {
+            string output = "";
+            int errorCode = NativeMethods.indy_vdr_prepare_txn_author_agreement_acceptance(
+                FfiStr.Create(text),
+                FfiStr.Create(version),
+                FfiStr.Create(taaDigest),
+                FfiStr.Create(accMechType),
+                time,
+                ref output);
 
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+
+            return output;
+        }
+
+        public static async Task RequestFree(
+            uint requestHandle)
+        {
+            string output = "";
+            int errorCode = NativeMethods.indy_vdr_request_free(
+                requestHandle);
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+        }
+
+        public static async Task<string> RequestGetBody(
+            uint requestHandle)
+        {
+            string output = "";
+            int errorCode = NativeMethods.indy_vdr_request_get_body(
+                requestHandle,
+                ref output);
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+            return output;
+        }
+
+        public static async Task<string> RequestGetSignatureInput(
+            uint requestHandle)
+        {
+            string output = "";
+            int errorCode = NativeMethods.indy_vdr_request_get_signature_input(
+                requestHandle,
+                ref output);
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+            return output;
+        }
+
+        public static async Task RequestSetEndorser(
+            uint requestHandle,
+            string endorser)
+        {
+            int errorCode = NativeMethods.indy_vdr_request_set_endorser(
+                requestHandle,
+                FfiStr.Create(endorser));
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+        }
+
+        public static async Task RequestSetMultiSignature(
+            uint requestHandle,
+            string identifier,
+            ByteBuffer signature)
+        {
+            int errorCode = NativeMethods.indy_vdr_request_set_multi_signature(
+                requestHandle,
+                FfiStr.Create(identifier),
+                signature);
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+        }
+
+        public static async Task RequestSetSiganture(
+            uint requestHandle,
+            ByteBuffer signature)
+        {
+            int errorCode = NativeMethods.indy_vdr_request_set_signature(
+                requestHandle,
+                signature);
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+        }
+
+        public static async Task RequestSetTxnAuthorAgreementAcceptance(
+            uint requestHandle,
+            string acceptance)
+        {
+            int errorCode = NativeMethods.indy_vdr_request_set_txn_author_agreement_acceptance(
+                requestHandle,
+                FfiStr.Create(acceptance));
+
+            if (errorCode != 0)
+            {
+                string error = "";
+                NativeMethods.indy_vdr_get_current_error(ref error);
+                Console.WriteLine(error);
+            }
+        }
     }
 }
