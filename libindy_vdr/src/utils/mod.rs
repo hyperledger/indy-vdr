c6f160871bdd48834e0e66aece852b2626d62ace
blu3beri <blu3beri@proton.me>
2023-01-03 14:56:06 +0100

    Added base64 and base58 inside the package
    Signed-off-by: blu3beri <blu3beri@proton.me>

--- libindy_vdr/src/utils/mod.rs
+++ libindy_vdr/src/utils/mod.rs
@@ -2,12 +2,11 @@
 mod macros;
 
 pub mod txn_signature;
+pub mod base64;
+pub mod base58;
 
 // re-exports
 pub use indy_utils::{qualifiable, ConversionError, Qualifiable, Validatable, ValidationError};
-
-pub(crate) use indy_utils::base58;
-pub(crate) use indy_utils::base64;
 pub use indy_utils::did;
 pub(crate) use indy_utils::hash;
 pub use indy_utils::keys;
