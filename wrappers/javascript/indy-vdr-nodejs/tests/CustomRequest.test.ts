b976bcd9c809b7ca961ef227e8ed29ca5e4368de
Timo Glastra <timo@animo.id>
2023-04-11 11:47:15 +0200

    fixes
    Signed-off-by: Timo Glastra <timo@animo.id>

--- wrappers/javascript/indy-vdr-nodejs/tests/CustomRequest.test.ts
+++ wrappers/javascript/indy-vdr-nodejs/tests/CustomRequest.test.ts
@@ -1,8 +1,8 @@
 import type { IndyVdrPool } from '@hyperledger/indy-vdr-nodejs'
 
-import { CustomRequest } from '@hyperledger/indy-vdr-nodejs'
-
 import { DID, setupPool } from './utils'
+
+import { CustomRequest } from '@hyperledger/indy-vdr-nodejs'
 
 describe('CustomRequest', () => {
   let pool: IndyVdrPool
@@ -24,7 +24,7 @@ describe('CustomRequest', () => {
     })
   })
 
-  test('Can parse a request from string', async () => {
+  test('Can parse a request from string', () => {
     const json = `{"endorser":"DJKobikPAaYWAu9vfhEEo5","identifier":"2GjxcxqE2XnFrVhipkWCWT","operation":{"dest":"2GjxcxqE2XnFrVhipkWCWT","raw":"{\\"endpoint\\":{\\"endpoint\\":\\"https://example.com/endpoint\\",\\"routingKeys\\":[\\"a-routing-key\\"],\\"types\\":[\\"endpoint\\",\\"did-communication\\",\\"DIDComm\\"]}}","type":"100"},"protocolVersion":2,"reqId":1680599092494999800,"signature":"3RyENWHC1szYH7FwDfZ2pKteShtsuDgYCSjGQGDPDjAYE5mipCZ6AnZKuAgCQYq6yt1LEfPPRKVS8BjBirX5s5q3","taaAcceptance":{"mechanism":"accept","taaDigest":"e546ad2a5311b2020fd80efb4d17ec75f823d26ee2424cf741ee345ede9d3ff3","time":1680566400}}`
     const request = new CustomRequest({
       customRequest: json,
