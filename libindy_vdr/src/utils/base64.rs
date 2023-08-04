1994640ee01086f9964570b966b88ecc2811b7f1
Andrew Whitehead <cywolf@gmail.com>
2023-08-04 14:03:33 -0700

    update base64 dependency
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- libindy_vdr/src/utils/base64.rs
+++ libindy_vdr/src/utils/base64.rs
@@ -1,18 +1,19 @@
-use base64_rs as base64;
-
-pub use base64::encode;
+use base64_rs::Engine;
 
 use super::ConversionError;
 
 pub fn decode<T: AsRef<[u8]>>(val: T) -> Result<Vec<u8>, ConversionError> {
-    Ok(base64::decode(val).map_err(|err| ("Error decoding base64 data", err))?)
+    Ok(base64_rs::engine::general_purpose::STANDARD
+        .decode(val)
+        .map_err(|err| ("Error decoding base64 data", err))?)
 }
 
 pub fn decode_urlsafe<T: AsRef<[u8]>>(val: T) -> Result<Vec<u8>, ConversionError> {
-    Ok(base64::decode_config(val, base64::URL_SAFE)
+    Ok(base64_rs::engine::general_purpose::URL_SAFE
+        .decode(val)
         .map_err(|err| ("Error decoding base64-URL data", err))?)
 }
 
 pub fn encode_urlsafe<T: AsRef<[u8]>>(val: T) -> String {
-    base64::encode_config(val, base64::URL_SAFE)
+    base64_rs::engine::general_purpose::URL_SAFE.encode(val)
 }
