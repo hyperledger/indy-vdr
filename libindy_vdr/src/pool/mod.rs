9a5fb16bb860b343f2d2cf71b0ad07b00fef8aa4
Andrew Whitehead <cywolf@gmail.com>
2023-08-08 10:57:28 -0700

    fix warning
    Signed-off-by: Andrew Whitehead <cywolf@gmail.com>

--- libindy_vdr/src/pool/mod.rs
+++ libindy_vdr/src/pool/mod.rs
@@ -4,10 +4,10 @@ mod genesis;
 pub(crate) mod handlers;
 /// Methods for performing requests against the verifier pool
 pub mod helpers;
+/// General verifier pool management
+mod manager;
 /// Pool networker traits and implementations
 pub mod networker;
-/// General verifier pool management
-mod pool;
 /// Data types and traits for handling pending verifier pool requests
 mod requests;
 /// A pool executor that processes events in its own thread
@@ -16,7 +16,7 @@ mod types;
 
 pub use self::builder::PoolBuilder;
 pub use self::genesis::PoolTransactions;
-pub use self::pool::{LocalPool, Pool, PoolImpl, SharedPool};
+pub use self::manager::{LocalPool, Pool, PoolImpl, SharedPool};
 pub use self::requests::{
     new_request_id, PoolRequest, PoolRequestImpl, PreparedRequest, RequestMethod,
 };
