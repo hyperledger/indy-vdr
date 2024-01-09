dfb9ff4fe562017178c35444316ec0c3667fa334
Berend Sliedrecht <sliedrecht@berend.io>
2024-01-09 21:18:51 +0100

    fix(js): type the revocation registry delta
    Signed-off-by: Berend Sliedrecht <sliedrecht@berend.io>

--- wrappers/javascript/indy-vdr-shared/src/builder/RevocationRegistryEntryRequest.ts
+++ wrappers/javascript/indy-vdr-shared/src/builder/RevocationRegistryEntryRequest.ts
@@ -2,22 +2,27 @@ import type { WriteRequestResultTxnBase, WriteRequestResponse } from '../types'
 
 import { indyVdr, IndyVdrRequest } from '../indyVdr'
 
+type ClRevocationRegistryDelta = {
+  prevAccum?: string
+  accum: string
+  issued: Array<number>
+  revoked: Array<number>
+}
+
 export type RevocationRegistryEntryRequestOptions = {
   submitterDid: string
   revocationRegistryDefinitionId: string
   revocationRegistryDefinitionType: string
   revocationRegistryEntry: {
     ver: '1.0'
-    value: { accum: string }
+    value: ClRevocationRegistryDelta | Record<string, unknown>
   }
 }
 
 interface RevocationRegistryEntryResultTxn extends WriteRequestResultTxnBase {
   type: '114'
   data: {
-    value: {
-      accum: string
-    }
+    value: ClRevocationRegistryDelta | Record<string, unknown>
     revocRegDefId: string
     revocDefType: 'CL_ACCUM'
   }
