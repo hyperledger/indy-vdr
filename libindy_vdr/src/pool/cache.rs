4478b2f60b725d72582ed2991b082ff690d63f60
wadeking98 <wkingnumber2@gmail.com>
2024-01-17 14:15:28 -0800

    finished writing tests
    Signed-off-by: wadeking98 <wkingnumber2@gmail.com>

--- libindy_vdr/src/pool/cache.rs
+++ libindy_vdr/src/pool/cache.rs
@@ -1,12 +1,11 @@
 use std::{
     collections::HashMap,
+    hash::Hash,
     sync::Arc,
     time::{SystemTime, UNIX_EPOCH},
 };
 
 use tokio::sync::RwLock;
-
-use super::RequestResultMeta;
 
 // cant use async traits yet because not object safe
 pub trait CacheStorage<K, V>: Send + Sync + 'static {
@@ -20,7 +19,7 @@ pub struct Cache<K, V> {
     expiration_offset: u64,
 }
 
-impl<K:'static, V: 'static> Cache<K, V> {
+impl<K: 'static, V: 'static> Cache<K, V> {
     pub fn new(storage: impl CacheStorage<K, V>, expiration_offset: u64) -> Self {
         Self {
             storage: Arc::new(RwLock::new(storage)),
@@ -68,11 +67,11 @@ impl<K, V> Clone for Cache<K, V> {
     }
 }
 
-pub struct MemCacheStorage {
-    cache: HashMap<String, (String, RequestResultMeta, u64)>,
+pub struct MemCacheStorage<K, V> {
+    cache: HashMap<K, (V, u64)>,
 }
 
-impl MemCacheStorage {
+impl<K, V> MemCacheStorage<K, V> {
     pub fn new() -> Self {
         Self {
             cache: HashMap::new(),
@@ -80,86 +79,39 @@ impl MemCacheStorage {
     }
 }
 
-impl CacheStorage<String, (String, RequestResultMeta)> for MemCacheStorage {
-    fn get(&self, key: &String) -> Option<((String, RequestResultMeta), u64)> {
-        self.cache
-            .get(key)
-            .map(|(v, m, e)| ((v.clone(), m.clone()), *e))
+impl<K: Hash + Eq + Send + Sync + 'static, V: Clone + Send + Sync + 'static> CacheStorage<K, V>
+    for MemCacheStorage<K, V>
+{
+    fn get(&self, key: &K) -> Option<(V, u64)> {
+        self.cache.get(key).map(|(v, e)| (v.clone(), *e))
     }
 
-    fn insert(
-        &mut self,
-        key: String,
-        value: (String, RequestResultMeta),
-        expiration: u64,
-    ) -> Option<((String, RequestResultMeta), u64)> {
+    fn insert(&mut self, key: K, value: V, expiration: u64) -> Option<(V, u64)> {
         self.cache
-            .insert(key, (value.0, value.1, expiration))
-            .map(|(v, m, e)| ((v.clone(), m.clone()), e))
+            .insert(key, (value, expiration))
+            .map(|(v, e)| (v.clone(), e))
     }
 }
 
-// pub trait Cacheable<K, V>: Clone + Send + Sync + 'static {
-//     fn get_cached_request(&self, key: K) -> impl Future<Output = Option<(V, RequestResultMeta)>>;
+#[cfg(test)]
+mod tests {
 
-//     fn cache_request(
-//         &mut self,
-//         key: K,
-//         result: V,
-//         meta: RequestResultMeta,
-//     ) -> impl Future<Output = Option<(V, RequestResultMeta)>>;
-// }
+    use futures_executor::block_on;
 
-// pub struct MemCache<K, V> {
-//     cache: Arc<RwLock<HashMap<K, (V, RequestResultMeta, u64)>>>,
-// }
+    #[rstest]
+    fn test_cache() {
+        use super::*;
+        use std::{time::Duration, thread};
 
-// impl<K, V> MemCache<K, V> {
-//     pub fn new() -> Self {
-//         Self {
-//             cache: Arc::new(RwLock::new(HashMap::new())),
-//         }
-//     }
-// }
-
-// need to implement Clone manually because RwLock<HashMap> doesn't implement Clone
-// impl<K, V> Clone for MemCache<K, V> {
-//     fn clone(&self) -> Self {
-//         Self {
-//             cache: self.cache.clone(),
-//         }
-//     }
-// }
-
-// impl<K: Hash + Eq + Send + Sync + 'static, V: Clone + Send + Sync + 'static> Cacheable<K, V>
-//     for MemCache<K, V>
-// {
-//     fn get_cached_request(&self, key: K) -> impl Future<Output = Option<(V, RequestResultMeta)>> {
-//         future::ready(match self.cache.read() {
-//             Ok(cache) => cache
-//                 .get(&key)
-//                 .and_then(|(v, m, _)| Some((v.clone(), m.clone()))),
-//             Err(err) => {
-//                 warn!("Error reading cache: {}", err);
-//                 None
-//             }
-//         })
-//     }
-
-//     fn cache_request(
-//         &mut self,
-//         key: K,
-//         result: V,
-//         meta: RequestResultMeta,
-//     ) -> impl Future<Output = Option<(V, RequestResultMeta)>> {
-//         future::ready(match self.cache.write() {
-//             Ok(mut cache) => cache
-//                 .insert(key, (result, meta, 0))
-//                 .and_then(|(v, m, _)| Some((v, m))),
-//             Err(err) => {
-//                 warn!("Error writing cache: {}", err);
-//                 None
-//             }
-//         })
-//     }
-// }
+        let mut cache = Cache::new(MemCacheStorage::new(), 1);
+        block_on(async {
+            cache.insert("key".to_string(), "value".to_string()).await;
+            assert_eq!(
+                cache.get(&"key".to_string()).await,
+                Some("value".to_string())
+            );
+            thread::sleep(Duration::from_secs(2));
+            assert_eq!(cache.get(&"key".to_string()).await, None);
+        });
+    }
+}
